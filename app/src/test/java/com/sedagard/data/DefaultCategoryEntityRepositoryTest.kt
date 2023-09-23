cfc1ec96e91f91a948e1f856cf49d70fb51d8817
Reza Moallemi <rm.allmails@gmail.com>
2023-09-23 10:31:27 -0700

    Add Project files

---
+++ app/src/test/java/com/sedagard/data/DefaultCategoryEntityRepositoryTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sedagard.data
+
+import com.sedagard.data.local.database.CategoryEntity
+import com.sedagard.data.local.database.CategoryEntityDao
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Unit tests for [DefaultCategoryEntityRepository].
+ */
+@OptIn(ExperimentalCoroutinesApi::class) // TODO: Remove when stable
+class DefaultCategoryEntityRepositoryTest {
+
+  @Test
+  fun categoryEntitys_newItemSaved_itemIsReturned() = runTest {
+    val repository = DefaultCategoryEntityRepository(FakeCategoryEntityDao())
+
+    repository.add("Repository")
+
+    assertEquals(repository.categoryEntitys.first().size, 1)
+  }
+}
+
+private class FakeCategoryEntityDao : CategoryEntityDao {
+
+  private val data = mutableListOf<CategoryEntity>()
+
+  override fun getCategoryEntitys(): Flow<List<CategoryEntity>> = flow {
+    emit(data)
+  }
+
+  override suspend fun insertCategoryEntity(item: CategoryEntity) {
+    data.add(0, item)
+  }
+}
