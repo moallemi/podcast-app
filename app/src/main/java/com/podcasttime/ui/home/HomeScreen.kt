7bb67287ac021e61460f9020880c4320eb7107a3
yasinhajilou <hajilouyasin@gmail.com>
2023-11-05 12:04:33 -0500

    Added: MockData for preview in HomeScreen

--- app/src/main/java/com/podcasttime/ui/home/HomeScreen.kt
+++ app/src/main/java/com/podcasttime/ui/home/HomeScreen.kt
@@ -3,6 +3,10 @@ package com.podcasttime.ui.home
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.podcasttime.data.HomePodcastRow
+import com.podcasttime.data.model.Category
+import com.podcasttime.data.model.Podcast
 import com.podcasttime.ui.home.components.PodcastItem
 
 @Composable
@@ -19,3 +23,38 @@ fun HomeScreen(
     }
   }
 }
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+  val mockState = HomeUiState(isLoading = false, mockHomePodcastRow)
+  HomeScreen(state = mockState, onPodcastClick = {})
+}
+
+val mockPodcasts = listOf(
+  Podcast(
+    id = "1",
+    title = "Sample 1",
+    image = "https://via.placeholder.com/150/56a8c2",
+  ),
+  Podcast(
+    id = "1",
+    title = "Sample 1",
+    image = "https://via.placeholder.com/150/56a8c2",
+  ),
+  Podcast(
+    id = "1",
+    title = "Sample 1",
+    image = "https://via.placeholder.com/150/56a8c2",
+  ),
+  Podcast(
+    id = "1",
+    title = "Sample 1",
+    image = "https://via.placeholder.com/150/56a8c2",
+  ),
+)
+
+val mockHomePodcastRow = listOf(
+  HomePodcastRow(Category("Check"), mockPodcasts),
+  HomePodcastRow(Category("Check2"), mockPodcasts),
+)
