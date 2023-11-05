a6d2457cb486a5cd05b747d2b6edf84074123e68
yasinhajilou <hajilouyasin@gmail.com>
2023-11-05 12:17:23 -0500

    Added: Top Appbar

--- app/src/main/java/com/podcasttime/ui/home/HomeScreen.kt
+++ app/src/main/java/com/podcasttime/ui/home/HomeScreen.kt
@@ -4,9 +4,17 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import com.podcasttime.data.HomePodcastRow
 import com.podcasttime.data.model.Category
@@ -21,6 +29,7 @@ fun HomeScreen(
   Scaffold(
     modifier = Modifier.fillMaxSize(),
     topBar = {
+      HomeTopAppBar()
     },
     content = { paddingValues ->
       HomeContent(
@@ -28,6 +37,25 @@ fun HomeScreen(
         onPodcastClick = onPodcastClick,
         paddingValues = paddingValues,
       )
+    },
+  )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopAppBar() {
+  CenterAlignedTopAppBar(
+    navigationIcon = {
+      IconButton(onClick = { /*TODO*/ }) {
+        Icon(
+          modifier = Modifier.rotate(180f),
+          imageVector = Icons.Default.ExitToApp,
+          contentDescription = "Exit the app",
+        )
+      }
+    },
+    title = {
+      Text(text = "Podcast Time")
     },
   )
 }
