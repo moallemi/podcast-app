05eb7518d8217d18c404893d22ed42cd0b0893db
yasinhajilou <hajilouyasin@gmail.com>
2023-11-06 08:48:03 -0500

    Modified: LazyHorizontalGrid instead of LazyRow

--- app/src/main/java/com/podcasttime/ui/home/components/PodcastItem.kt
+++ app/src/main/java/com/podcasttime/ui/home/components/PodcastItem.kt
@@ -3,18 +3,23 @@ package com.podcasttime.ui.home.components
 import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.podcasttime.data.HomePodcastRow
@@ -47,30 +52,40 @@ private fun PodcastList(
   podcasts: List<Podcast>,
   onPodcastClick: (podcastId: String) -> Unit,
 ) {
-  LazyRow(
+  LazyHorizontalGrid(
+    modifier = Modifier.height(((192 * 2) + 8).dp),
+    rows = GridCells.Fixed(2),
     contentPadding = PaddingValues(16.dp),
-    horizontalArrangement = spacedBy(8.dp),
+    horizontalArrangement = spacedBy(24.dp),
+    verticalArrangement = spacedBy(8.dp),
   ) {
     items(podcasts) { podcast ->
       Card(
         modifier = Modifier
-          .width(128.dp),
+          .width(128.dp)
+          .height(192.dp),
         onClick = { onPodcastClick(podcast.id) },
       ) {
         Column {
           AsyncImage(
             modifier = Modifier
-              .size(128.dp),
+              .width(128.dp)
+              .weight(4f),
             model = podcast.image,
             contentDescription = null,
+            contentScale = ContentScale.FillBounds,
           )
           Text(
             modifier = Modifier
-              .padding(8.dp),
+              .padding(8.dp)
+              .fillMaxWidth()
+              .weight(1f),
             text = podcast.title,
             style = MaterialTheme.typography.bodyMedium,
             minLines = 2,
             maxLines = 2,
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Ellipsis,
           )
         }
       }
